b6067316d2db8707bf99c67b6794c5085f3fec35
Benjamin Freoua <89775252+Benjamin-Freoua-Alma@users.noreply.github.com>
2021-10-04 15:50:22 +0200

    PestTest init

---
+++ alma/tests/BenTest.php
@@ -0,0 +1,13 @@
+<?php
+
+test('test of ben', function() {
+    $this->assertTrue(true);
+
+    expect(true)->toBeTrue();
+});
+
+it('it of ben', function() {
+    $this->assertTrue(true);
+
+    expect(true)->toBeTrue();
+});
